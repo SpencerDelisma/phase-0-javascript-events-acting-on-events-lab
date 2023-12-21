898a68d860771a09fb56e8632fb1b6b2920f509e
Delisma <delispence7@gmail.com>
2023-12-20 23:34:24 -0500

    added

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4"
+dodger.style.bottom = "100px";
+dodger.style.left = "0px";
+document.addEventListener("keydown", function (event) {
+  console.log(event);
+});
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  const gameWidth = 400; // replace with the actual width of the game field
+
+  if (left + 40 < gameWidth) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
